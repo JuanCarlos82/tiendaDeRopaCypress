0ff62b8f2d4b9b59b5f8f168bbeead1c96590b74
JuanCarlos82 <juan.gonzalez@salud.gob.sv>
2022-05-12 08:35:33 -0600

    actualizacion_12_05_2022

--- cypress/integration/search.js
+++ cypress/integration/search.js
@@ -1,12 +1,23 @@
 describe ('Search elements',()=>{
-    it('search for elements with mulriple results',()=>{
+    beforeEach(()=>{
         cy.visit('/');
+    })
+    it('search for elements with mulriple results',()=>{
         cy.fixture('index').then((index)=>{
             cy.get(index.searchBox).type('dress');
             cy.get(index.searchButton).click();
         })
         cy.fixture('searchResult').then((searchResult)=>{
-            cy.get(searchResult.title).should('contain','dress')
+        cy.get(searchResult.title).should('contain','dress');
+        })
+    })
+    it('Search for elements with no results',()=>{
+        cy.fixture('index').then((index)=>{
+            cy.get(index.searchBox).type('qwerty');
+            cy.get(index.searchButton).click();
+        }) 
+        cy.fixture('searchResult').then((searchResult)=>{
+        cy.get(searchResult.alert).should('contain','No results');
         })
     })
 })
